adb3ee568f623b00fbc2a2965070ce8ed6699dd1
PedroHBelini <pedroh.belini@outlook.com>
2024-09-24 15:42:37 -0300

    commit -m "Adiciona lógica de inserir/remover números"

--- src/ConjuntoNumeros.java
+++ src/ConjuntoNumeros.java
@@ -13,7 +13,54 @@ public class ConjuntoNumeros {
         }
 
         imprimirVetor(vetor, tamanho);
+
+        while (tamanho > 0 && tamanho < 10) {
+
+            System.out.print("> ");
+
+            int numero = scanner.nextInt();
+
+            int indice = encontrarIndice(vetor, tamanho, numero);
+
+            if (indice == -1) {
+                if (tamanho < 10) {
+
+                    vetor[tamanho] = numero;
+
+                    tamanho++;
+
+                }
+
+            } else {
+
+                for (int i = indice; i < tamanho - 1; i++) {
+
+                    vetor[i] = vetor[i + 1];
+
+                }
+                tamanho--;
+
+            }
+            imprimirVetor(vetor, tamanho);
+
+        }
+
         scanner.close();
+    }
+    private static int encontrarIndice(int[] vetor, int tamanho, int numero) {
+
+        for (int i = 0; i < tamanho; i++) {
+
+            if (vetor[i] == numero) {
+
+                return i;
+
+            }
+
+        }
+
+        return -1;
+
     }
     private static void imprimirVetor(int[] vetor, int tamanho) {
         for (int i = 0; i < tamanho; i++) {
